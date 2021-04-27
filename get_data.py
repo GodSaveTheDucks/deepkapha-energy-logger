05e3c4d2eae3b181988c8209d628c76126a5a75b
GodSaveTheDucks <suhas@claire.ai>
2021-04-27 15:46:10 +0530

    updated

--- get_data.py
+++ get_data.py
@@ -8,13 +8,14 @@ class GetDataController(object):
             'wind_farm_data': 'https://www.thecrownestate.co.uk/api/energy-map/wind-farm-data',}
     
     def generate_file_name(self):
-        return str(datetime.datetime.now().timestamp())
+        return str(datetime.datetime.utcnow())
+
         
     def execute(self,data_dir='data'):
         for filename, endpoint_url in self.urls.items():
             r = requests.get(endpoint_url)
-            filename = ''.join([filename,generate_file_name()])
-            with open(f'{data_dir}/{filename}.json', 'w') as fp:
+            filename = ''.join([filename,"-",self.generate_file_name()])
+            with open(f'{data_dir}/{filename}.json','w+') as fp:
                 json.dump(r.json(), fp) 
 
 controller = GetDataController()
